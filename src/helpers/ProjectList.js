a0bc6c7119fa0a522026468c91139a3ded506a91
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-25 13:46:16 +0000

    ids given to each item in list

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -7,36 +7,42 @@ import consoleFinances from '../assets/consoleFinances.png'
 
 export const ProjectList = [
     {
+        id: 0,
         name: "Travel App",
         image: travelApp,
         deployed: "https://michalous.github.io/travel-app/",
         GitHub: "https://github.com/Michalous/travel-app",
     },
     {
+        id: 1,
         name: "Bootstrap4 Portfolio",
         image: bsPortfolio,
         deployed: "https://monicaclarelewis.github.io/Monica-Lewis-Portfolio/",
         GitHub: "https://github.com/monicaclarelewis/Monica-Lewis-Portfolio",
     },
     {
+        id: 2,
         name: "Work Scheduler",
         image: workScheduler,
         deployed: "https://monicaclarelewis.github.io/work-scheduler/",
         GitHub: "https://github.com/monicaclarelewis/work-scheduler",
     },
     {
+        id: 3,
         name: "Coding Quiz",
         image: quiz,
         deployed: "https://monicaclarelewis.github.io/Code-Quiz/index.html",
         GitHub: "https://github.com/monicaclarelewis/Code-Quiz",
     },
     {
+        id: 4,
         name: "Password Generator",
         image: passwordG,
         deployed: "https://monicaclarelewis.github.io/password-generator/",
         GitHub: "https://github.com/monicaclarelewis/password-generator",
     },
     {
+        id: 5,
         name: "Console Finances",
         image: consoleFinances,
         deployed: "https://monicaclarelewis.github.io/Finances-using-JavaScript//",
