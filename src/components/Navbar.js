09c56876e2bbe4b0d877927afa9f2d5d0e51379f
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-25 09:52:12 +0000

    updating class name tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ import "../styles/Navbar.css";
 
 function Navbar () {
   return (
-    <div class="navbar navbar-expand-lg customNav navMain navbar-custom myNav">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    <div className="navbar navbar-expand-lg customNav navMain navbar-custom myNav">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <Link to="/"> Home </Link>
             <Link to="/projects"> Projects </Link>
             <Link to="/contact"> Contact </Link>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto links navigate">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto links navigate">
             <Link to="/"> Home </Link>
             <Link to="/projects"> Projects </Link>
             <Link to="/contact"> Contact </Link>
